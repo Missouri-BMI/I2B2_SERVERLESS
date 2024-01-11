9061c108e9720f70974ea67cc835bc0e799acbba
Md Saber Hossain <mhmcb@missouri.edu>
2024-01-11 13:25:05 -0600

    [TASK]: shrine configuration and set up in docker

---
+++ docker/shrine-server/src/executables/commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo 'ping to shrine hub ip address.........'
+curl -kw "\n%{response_code}\n" -X GET https://shrine-act-test.hms.harvard.edu:6443/shrine-api/hub/ping
+
+
+/usr/local/tomcat/bin/catalina.sh run -d
+
+
