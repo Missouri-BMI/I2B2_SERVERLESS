e2ac0da673541c2bacd7a59fbb56a5ea456e6ceb
Md Saber Hossain <mhmcb@missouri.edu>
2024-01-11 13:25:05 -0600

    [TASK]: shrine configuration and set up in docker

---
+++ docker/pass-gen/gen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+javac PasswordUtils.java && \
+java PasswordUtils random@umsystem.edu
---
+++ docker/shrine-server/src/executables/setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set Tomcat options
+export CATALINA_OPTS="$CATALINA_OPTS -server -Xms1024m -Xmx3072m -Duser.timezone=America/New_York"
