680ac52e54efde9a446b9f153e0a7e99973fd6b1
Md Saber Hossain <mhmcb@missouri.edu>
2024-04-23 11:07:35 -0500

    wip..

--- docker/i2b2-server/build.sh
+++ docker/i2b2-server/build.sh
@@ -6,3 +6,8 @@ docker build  \
 --build-arg ENVIRONMENT=shrine \
 --no-cache .
 
+# docker build  \
+# --platform=linux/amd64 \
+# -t 500206249851.dkr.ecr.us-east-2.amazonaws.com/i2b2-wildfly:shrine-washu \
+# --build-arg ENVIRONMENT=shrine-washu \
+# --no-cache .
