25228d9a2634000c90d819d724c745ad9eeeb36d
Md Saber Hossain <mhmcb@missouri.edu>
2024-05-21 10:35:48 -0500

    [WIP]: saml shrine implementation

--- docker/shrine-server/src/executables/commands.sh
+++ docker/shrine-server/src/executables/commands.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 
-echo 'ping to shrine hub ip address.........'
-curl -kw "\n%{response_code}\n" -X GET https://shrine-act-test.hms.harvard.edu:6443/shrine-api/hub/ping
+# echo 'ping to shrine hub ip address.........'
+# curl -kw "\n%{response_code}\n" -X GET https://shrine-act-test.hms.harvard.edu:6443/shrine-api/hub/ping
 
 
+# Start Shibd
+/etc/shibboleth/shibd-redhat start
+
+# Start httpd
+# exec httpd -DFOREGROUND
+
+# Start tomcat shrine
 /usr/local/tomcat/bin/catalina.sh run -d
