5957e28093fa8548e268da182e7996e1702abaa1
Md Saber Hossain <mhmcb@missouri.edu>
2024-06-11 10:26:04 -0500

    shrine prod

--- docker/i2b2-server/build-prod.sh
+++ docker/i2b2-server/build-prod.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# docker build  \
+# --platform=linux/amd64 \
+# -t 063312575449.dkr.ecr.us-east-2.amazonaws.com/i2b2-wildfly:prod \
+# --build-arg ENVIRONMENT=prod \
+# --no-cache .
+
+
 docker build  \
 --platform=linux/amd64 \
--t 063312575449.dkr.ecr.us-east-2.amazonaws.com/i2b2-wildfly:prod \
---build-arg ENVIRONMENT=prod \
+-t 063312575449.dkr.ecr.us-east-2.amazonaws.com/i2b2-wildfly:shrine-mu \
+--build-arg ENVIRONMENT=shrine-prod \
 --no-cache .
+
+
+# docker build  \
+# --platform=linux/amd64 \
+# -t 063312575449.dkr.ecr.us-east-2.amazonaws.com/i2b2-wildfly:shrine-washu \
+# --build-arg ENVIRONMENT=shrine-washu-prod \
+# --no-cache .
